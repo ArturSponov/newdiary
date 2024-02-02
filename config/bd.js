da3ea43d56b7cf6b04ec101e706d030d1597ccd0
ArturSponov <129497233+ArturSponov@users.noreply.github.com>
2024-02-02 14:23:37 +0600

    Update bd.js

--- config/bd.js
+++ config/bd.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoUrl: 'mongodb://127.0.0.1:27017/Users',
+    mongoUrl: 'mongodb+srv://kamartur778:oPa0j6q0YXecSdPS@cluster0.pxcvidg.mongodb.net/?retryWrites=true&w=majority',
     
     
     // mongodb+srv://kamartur778:oPa0j6q0YXecSdPS@cluster0.pxcvidg.mongodb.net/?retryWrites=true&w=majority",
